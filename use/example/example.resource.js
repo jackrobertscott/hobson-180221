623fb0678946ea6776a8b14c474066e7dcf6301a
Jack Scott <mast.j.scott@gmail.com>
2018-02-21 18:50:13 +0800

    fix the smack

--- use/example/example.resource.js
+++ use/example/example.resource.js
@@ -6,12 +6,7 @@ const endpoints = new Map();
 endpoints.set('smacktalk', {
   path: '/smack/talk',
   method: 'get',
-  handler: async () => ({ talk: 'Yo mama!' }),
-  activate: [
-    async () => {
-      // throw new Error('Lalalala');
-    },
-  ],
+  handler: async () => ({ talk: ['Yo mama!', 'Have 5 entries, or what!', 'Mwahahah word.', 'Blah', 'Hello! Smelly.'][Math.floor(Math.random() * 5)] }),
 });
 
 module.exports = new Resource('example', schema, { endpoints });
