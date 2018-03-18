d53fd2b86073084829aa9a07fbaa4a130d666f92
Jack Scott <jack.rob.scott@gmail.com>
2018-03-18 16:23:18 +0800

    do not include password by default

--- src/user.resource.js
+++ src/user.resource.js
@@ -16,6 +16,7 @@ class UserResource extends Resource {
       password: {
         type: String,
         required: true,
+        select: false,
       },
     });
     this.schema.pre('save', function preSave(next) {
