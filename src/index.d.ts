c2ab5e11231d20ade9ce7f6f35513b1dc4fcdb03
Thomas Rayden <thomasmwrayden@gmail.com>
2018-03-30 16:47:31 +0800

    few little fixes

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,13 +3,12 @@
 
 ///<reference types="node" />
 
-import core from './index';
 import { Mongoose } from 'mongoose';
 
 declare namespace hobson {
   interface Resource {
     /**
-     * Constructor
+     * Resource constructor
      */
     ({
       name: string,
@@ -33,42 +32,31 @@ declare namespace hobson {
 
     /**
      * 
-     * @param id 
      */
     route(id: string): Route;
 
     /**
      * 
-     * @param id 
-     * @param endpoint 
      */
     addEndpoint(id: string, endpoint: object): Resource;
 
     /**
      * 
-     * @param id 
-     * @param middleware 
      */
     addMiddleware(id: string, middleware: function): Resource;
 
     /**
      * 
-     * @param id 
-     * @param hook 
      */
     addPreHook(id: string, hook: function): Resource;
 
     /**
      * 
-     * @param id 
-     * @param hook 
      */
     addPostHook(id: string, hook: function): Resource;
 
     /**
      * 
-     * @param id 
-     * @param permission 
      */
     addPermission(id: string, permission: function): Resource;
 
@@ -79,13 +67,22 @@ declare namespace hobson {
 
     /**
      * 
-     * @param app 
      */
     attach(app: Express): void;
   }
-  interface UserResource extends core.UserResouce {}
-  interface TokenResource extends core.TokenResource {}
-  interface ResponseError extends core.ResponseError {}
-  interface connect extends core.connect {}
-  interface access extends core.access {}
+  interface UserResource {
+
+  }
+  interface TokenResource {
+
+  }
+  interface ResponseError {
+
+  }
+  interface connect {
+
+  }
+  interface access {
+    
+  }
 }
