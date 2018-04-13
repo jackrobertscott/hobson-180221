580fed693717a094d98531c2852f0ff7a8f3fed5
Jack Scott <jack.rob.scott@gmail.com>
2018-04-13 16:31:52 +0800

    export sub resources

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 require('babel-polyfill');
 
-module.exports.Schema = require('./schema');
-module.exports.Route = require('./route');
-module.exports.Resource = require('./resource');
-
 module.exports.attach = require('./attach');
 module.exports.create = require('./create');
 module.exports.access = require('./access');
 module.exports.errors = require('./errors');
+
+module.exports.Schema = require('./schema');
+module.exports.Route = require('./route');
+module.exports.Resource = require('./resource');
+
+module.exports.TokenSchema = require('./token/schema');
+module.exports.UserResource = require('./user/resource');
+module.exports.UserSchema = require('./user/schema');
